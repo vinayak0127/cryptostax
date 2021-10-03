e86d63e11d4fdda9cfcd429bff265ef45c8f22d0
vinayak0127 <vinayakrajbusiness@gmail.com>
2021-10-03 16:32:57 +0530

    UI Commits

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -9,6 +9,7 @@ import {
   FormControl,
   Input,
   Button,
+  Grid,
 } from '@chakra-ui/react';
 
 import { useAuth } from '../../hooks/useAuth';
@@ -30,41 +31,43 @@ const LoginForm = () => {
   };
 
   return (
-    <GridItem
-      colStart={[1, null, null, 2, null, null]}
-      colSpan={[3, null, null, 1, null, null]}
-      p={6}
-    >
-      <Heading as="h1" mb={6}>
-        Login
-      </Heading>
-      {errors.email && (
-        <Alert status="error" variant="subtle" mt={6} mb={6}>
-          <AlertIcon />
-          {errors.email.message}
-        </Alert>
-      )}
-      {formState.isSubmitSuccessful && (
-        <Alert status="success" variant="subtle" mt={6} mb={6}>
-          <AlertIcon />
-          Check your email to complete login!
-        </Alert>
-      )}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormControl>
-          <FormLabel htmlFor="email">Email</FormLabel>
-          <Input name="email" placeholder="Email" ref={register()} />
-          <Button
-            mt={4}
-            colorScheme="teal"
-            isLoading={formState.isSubmitting}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </FormControl>
-      </form>
-    </GridItem>
+    <Grid>
+      <GridItem
+        colStart={[1, null, null, 2, null, null]}
+        colSpan={[3, null, null, 1, null, null]}
+        p={6}
+      >
+        <Heading as="h1" mb={6}>
+          Login
+        </Heading>
+        {errors.email && (
+          <Alert status="error" variant="subtle" mt={6} mb={6}>
+            <AlertIcon />
+            {errors.email.message}
+          </Alert>
+        )}
+        {formState.isSubmitSuccessful && (
+          <Alert status="success" variant="subtle" mt={6} mb={6}>
+            <AlertIcon />
+            Check your email to complete login!
+          </Alert>
+        )}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input name="email" placeholder="Email" ref={register()} />
+            <Button
+              mt={4}
+              colorScheme="teal"
+              isLoading={formState.isSubmitting}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </FormControl>
+        </form>
+      </GridItem>
+    </Grid>
   );
 };
 
